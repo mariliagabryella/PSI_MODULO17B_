2144292c6db4cd77372d4b124239938e9ebf1a10
GustavoAndréFontoura <al.919783@aeaav.pt>
2026-01-07 17:03:15 +0000

    Commit 2

--- M17BEx1/M17BEx1/M17BEx1/Program.cs
+++ M17BEx1/M17BEx1/M17BEx1/Program.cs
@@ -12,29 +12,27 @@ namespace M17BEx1
         {
             int[] numbers = { 5, 12, -3, 17, 42, 42, 0, -1, 100 };
             int sum = 0;
-            int max = 0;
+            int max = numbers[0];
             int min = numbers[0];
             int count42 = 0;
-            for (int i = 0; i <= numbers.Length; i++) {
+            for (int i = 0; i < numbers.Length; i++)
+            {
                 int n = numbers[i];
                 sum += n;
-                if (n > max) {
+                if (n > max)
                     max = n;
-                }
-                if (n < min) {
+                if (n < min)
                     min = n;
-                    if (n == 42)
-                    {
-                        count42++;
-                    }
-                }
-                double average = (double)sum / (numbers.Length - 1);
-                Console.WriteLine("Sum: " + sum);
-                Console.WriteLine("Average: " + average);
-                Console.WriteLine("Max " + max);
-                Console.WriteLine("Min: " + min);
-                Console.WriteLine("Count of 42: " + count42);
+                if (n == 42)
+                    count42++;
             }
+            double average = (double)sum / numbers.Length;
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Average: " + average);
+            Console.WriteLine("Max: " + max);
+            Console.WriteLine("Min: " + min);
+            Console.WriteLine("Count of 42: " + count42);
         }
     }
 }
+
